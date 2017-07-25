25173576859db5b740f82e98282ffb24fc9153e4
Ji Lu <jilu1@magento.com>
2017-07-25 09:40:55 -0500

    MQE-212: Implemented page object in memory.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,15 @@
 <?php
-// This is global bootstrap for autoloading
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+//Load constants from .env file
+$env = new \Dotenv\Loader(dirname(__DIR__) . '/.env');
+$env->load();
+
+foreach ($_ENV as $key => $var) {
+    defined($key) || define($key, $var);
+}
+
+defined('TESTS_BP') || define('TESTS_BP', dirname(__DIR__));
+defined('FW_BP') || define('FW_BP', TESTS_BP . '/vendor/magento/magento2-acceptance-test-framework');
+defined('TESTS_MODULE_PATH') || define('TESTS_MODULE_PATH', TESTS_BP . '/tests/acceptance/Magento/AcceptanceTest');
